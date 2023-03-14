f650f2b42fd5eda223900ffe673358b9223ff2b1
Daniel-Slavnyi <dslavnyj00@gmail.com>
2023-03-14 22:20:27 +0200

    finish: product page, sideBar

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,75 +1,68 @@
-import { Formik, Form, Field, useFormikContext } from 'formik';
-import { TextField, Button, Container } from '@mui/material';
+import React, { useEffect } from 'react';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
-
+import { Box, Button, TextField } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProductByTtn } from 'redux/product/product-operation';
-import { getProductTTN } from 'redux/product/product-selector';
 import { addToStory } from 'redux/product/product-slice';
-import { useEffect, useState } from 'react';
+import { getProductTTN } from 'redux/product/product-selector';
 
-const schema = Yup.object().shape({
-  ttn: Yup.string()
-    .matches(/^\d+$/, 'ТТН може містити тільки цифри')
-    .length(14, 'ТТН має містити рівно 14 цифр')
-    .required(),
-});
-
-export default function SearchForm({ getItems }) {
-  console.log('1', getItems.number);
-
+export default function SearchForm() {
   const dispatch = useDispatch();
+  const getItems = useSelector(getProductTTN);
 
-  const handleSubmit = (values, { resetForm }) => {
-    dispatch(getProductByTtn(values.ttn));
-    dispatch(addToStory(values.ttn));
-  };
+  const formik = useFormik({
+    initialValues: {
+      ttn: '',
+    },
+    validationSchema: Yup.object().shape({
+      ttn: Yup.string()
+        .matches(/^\d+$/, 'ТТН може містити тільки цифри')
+        .length(14, 'ТТН має містити рівно 14 цифр')
+        .required(),
+    }),
+    onSubmit: value => {
+      dispatch(getProductByTtn(value.ttn));
+      dispatch(addToStory(value.ttn));
+    },
+  });
 
-  console.log('2', getItems.number);
+  useEffect(() => {
+    if (!getItems.Number) {
+      return;
+    }
+    console.log('hihihi', getItems.Number);
+    formik.setFieldValue('ttn', getItems.Number);
+    formik.setValues({ ttn: getItems.Number });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [getItems.Number]);
 
   return (
-    <Container>
-      <Formik
-        initialValues={{ ttn: '' }}
-        onSubmit={handleSubmit}
-        validationSchema={schema}
-        validateOnChange={true}
-      >
-        {({ handleSubmit }) => (
-          <Form
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              gap: '20px',
-              marginTop: '20px',
-            }}
-            onSubmit={handleSubmit}
-          >
-            <p>{getItems.Number}</p>
-            <Field name="ttn">
-              {({ field, meta }) => (
-                <TextField
-                  label="TTN"
-                  {...field}
-                  error={meta.touched && Boolean(meta.error)}
-                  helperText={
-                    meta.touched && meta.error
-                      ? 'ТТН має містити рівно 14 цифр'
-                      : ''
-                  }
-                  style={{
-                    borderBottom:
-                      meta.touched && meta.error ? '2px solid red' : 'none',
-                  }}
-                />
-              )}
-            </Field>
-            <Button type="submit" variant="contained">
-              Get status TTN
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </Container>
+    <Box
+      component="form"
+      onSubmit={formik.handleSubmit}
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '20px',
+        marginTop: '20px',
+      }}
+    >
+      <TextField
+        fullWidth
+        label="TTN"
+        id="ttn"
+        name="ttn"
+        variant="outlined"
+        value={formik.values.ttn}
+        onChange={formik.handleChange}
+        helperText={formik.touched && formik.errors ? formik.errors.ttn : ''}
+        error={formik.touched && Boolean(formik.errors.ttn)}
+      />
+      <Button fullWidth type="submit" variant="contained">
+        Get status TTN
+      </Button>
+    </Box>
   );
 }
