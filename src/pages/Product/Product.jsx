731c378749a7013823cd2759696e6cf141753e5b
Daniel-Slavnyi <dslavnyj00@gmail.com>
2023-03-15 12:49:28 +0200

    update: card, drawer, searchForm

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -2,16 +2,12 @@ import { Container } from '@mui/material';
 import CardOfProduct from 'components/CardOfProduct/CardOfProduct';
 import SearchForm from 'components/SearchForm/SearchForm';
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { getProductTTN } from 'redux/product/product-selector';
 
 export default function Product() {
-  const getItems = useSelector(getProductTTN);
-
   return (
     <>
       <Container>
-        <SearchForm getItems={getItems} />
+        <SearchForm />
         <CardOfProduct />
       </Container>
     </>
